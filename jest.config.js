24cfb39ad207a1624b249112d1b29bea4c54642d
jpb06 <jpb.06@outlook.com>
2021-03-07 14:22:51 +0100

    Project init

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const {
+  compilerOptions: { paths: tsconfigPaths },
+} = require("./tsconfig");
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(tsconfigPaths, { prefix: "<rootDir>/src" }),
+  },
+  coveragePathIgnorePatterns: [
+    ".d.ts", ".js",
+    "<rootDir>/src/tests-related/",
+    "<rootDir>/src/types/",
+    "<rootDir>/src/pages/",
+  ],
+  collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+  coverageReporters: ["json-summary", "text", "lcov"],
+};
