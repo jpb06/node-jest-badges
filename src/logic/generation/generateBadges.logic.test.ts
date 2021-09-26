838e018a0ba3be493e4a41edc84b7e21ab90d9ef
jpb06 <jpb.06@outlook.com>
2021-09-26 21:59:39 +0200

    Reordering calls

--- src/logic/generation/generateBadges.logic.test.ts
+++ src/logic/generation/generateBadges.logic.test.ts
@@ -1,4 +1,5 @@
 import { emptyDir, ensureDir, readJson } from 'fs-extra';
+import { mocked } from 'ts-jest/utils';
 
 import { generateBadges } from './generateBadges.logic';
 import { generateCoverageFile } from './generateCoverageFile.logic';
@@ -7,12 +8,32 @@ jest.mock('fs-extra');
 jest.mock('./generateCoverageFile.logic');
 
 describe('generateBadges function', () => {
-  it('should generate all badges', async () => {
+  global.console = { error: jest.fn() } as unknown as Console;
+
+  beforeEach(() => jest.clearAllMocks());
+
+  it('should ensure outDir is there and clear it', async () => {
     await generateBadges();
 
     expect(ensureDir).toHaveBeenCalledTimes(1);
     expect(emptyDir).toHaveBeenCalledTimes(1);
+  });
+
+  it('should generate all badges', async () => {
+    await generateBadges();
+
     expect(readJson).toHaveBeenCalledTimes(1);
     expect(generateCoverageFile).toHaveBeenCalledTimes(5);
+    expect(console.error).toHaveBeenCalledTimes(0);
+  });
+
+  it('should report on errors', async () => {
+    mocked(readJson).mockRejectedValueOnce('Oh no!' as never);
+
+    await generateBadges();
+
+    expect(readJson).toHaveBeenCalledTimes(1);
+    expect(generateCoverageFile).toHaveBeenCalledTimes(0);
+    expect(console.error).toHaveBeenCalledTimes(1);
   });
 });
--- src/logic/generation/generateBadges.logic.ts
+++ src/logic/generation/generateBadges.logic.ts
@@ -1,15 +1,16 @@
 import { outputDir, summaryPath } from '@constants/fileSystem.constants';
-import { Summary } from '@owntypes/summary.type';
+import { Summary } from '@type/summary.type';
 import { emptyDir, ensureDir, readJson } from 'fs-extra';
 
 import { generateCoverageFile } from './generateCoverageFile.logic';
 
 export const generateBadges = async (): Promise<void> => {
-  const summary = (await readJson(summaryPath)) as Summary;
-
   await ensureDir(outputDir);
   await emptyDir(outputDir);
+
   try {
+    const summary = (await readJson(summaryPath)) as Summary;
+
     await generateCoverageFile(summary, 'global coverage');
     await generateCoverageFile(summary, 'lines');
     await generateCoverageFile(summary, 'statements');
