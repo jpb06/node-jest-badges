be852653fc021ee4c0dae29c604ecc3b03188c11
jpb06 <jpb.06@outlook.com>
2021-12-16 00:49:23 +0100

    Switching to babel

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { pathsToModuleNameMapper } = require("ts-jest");
 const {
   compilerOptions: { paths: tsconfigPaths },
 } = require("./tsconfig");
@@ -6,8 +6,11 @@ const {
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 
 module.exports = {
-  roots: ['<rootDir>/src/'],
-  preset: "ts-jest",
+  logHeapUsage: true,
+  transform: {
+    '^.+\\.ts$': ['babel-jest', { presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'] }],
+  },
+  moduleFileExtensions: ["js", "json", "ts"],
   watchPlugins: [
     "jest-watch-typeahead/filename",
     "jest-watch-typeahead/testname",
@@ -24,7 +27,8 @@ module.exports = {
     "!<rootDir>/dist/",
     "!<rootDir>/src/index.ts",
     "!<rootDir>/src/cli/generateBadges.cli.ts",
-    "!<rootDir>/src/tests-related/**"
+    "!<rootDir>/src/tests-related/**",
+    "!<rootDir>/src/types/**"
   ],
   modulePathIgnorePatterns: ['<rootDir>/dist'],
   moduleNameMapper: pathsToModuleNameMapper(tsconfigPaths, { prefix: '<rootDir>/src' }),
